789cf0a853253eea67a58ed3744bc4ee6d782a5b
BitcoinManor <62268768+BitcoinManor@users.noreply.github.com>
2025-04-13 01:57:46 -0600

    Declare shared label pointers for modular screen rendering

--- firmware/metrics_screen.h
+++ firmware/metrics_screen.h
@@ -1,6 +1,31 @@
-#ifndef METRICS_SCREEN_H
-#define METRICS_SCREEN_H
+// BLOKDBIT_Spark metrics_screen.h
+// SPARKv0.03
 
-void initMetricsScreen();
 
-#endif
+#pragma once
+#include <lvgl.h>
+
+
+// Shared state and controls
+extern bool isLeftOn;
+extern lv_obj_t* priceValueLabel;
+extern lv_obj_t* blockValueLabel;
+extern lv_obj_t* feeValueLabel;
+extern lv_obj_t* solvedByValueLabel;
+extern lv_obj_t* leftBtn;
+extern lv_obj_t* rightBtn;
+
+
+// Shared event handler
+extern void onTouchEvent(lv_event_t* e);
+
+// External shared styles and labels 
+extern lv_style_t widgetStyle;
+extern lv_style_t widget2Style;
+extern lv_style_t glowStyle;
+extern lv_style_t orangeStyle;
+extern lv_style_t greenStyle;
+extern lv_style_t blueStyle;
+
+lv_obj_t* create_metrics_screen();
+//void initMetricsScreen();
