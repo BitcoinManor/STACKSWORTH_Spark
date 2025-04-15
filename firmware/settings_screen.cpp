bd6cb539ad4a73e4eece4464c5f8937f88c9add4
BitcoinManor <62268768+BitcoinManor@users.noreply.github.com>
2025-04-15 03:02:27 -0600

    Add settings screen logic
    Implements the layout and navigation logic for the Settings screen, including the back button to return to the main metrics screen.

---
+++ firmware/settings_screen.cpp
@@ -0,0 +1,101 @@
+// BLOKDBIT_Spark settings_screen.cpp
+// SPARKv0.03
+
+
+#include <Arduino.h>
+#include "settings_screen.h"
+#include "screen_manager.h"
+
+
+//External shared Objects
+extern lv_obj_t* backBtn;
+extern lv_obj_t* rightBtn;
+
+
+// External shared styles and labels 
+extern lv_style_t widgetStyle;
+extern lv_style_t widget2Style;
+extern lv_style_t glowStyle;
+extern lv_style_t orangeStyle;
+extern lv_style_t greenStyle;
+extern lv_style_t blueStyle;
+
+//bool isRightOn = false;
+
+
+// Handle button press events
+void onTouchEvent_settings_screen(lv_event_t* e) {
+  lv_obj_t* target = lv_event_get_target(e);
+
+  if (target == backBtn) {
+    //isLeftOn = !isLeftOn;
+    Serial.println("➡️ Back button pressed: Switching to Metrics Screen");
+    load_screen(3);
+  } else if (target == rightBtn) {
+    Serial.println("➡️ Right button pressed: Switching to new screen");
+    load_screen(0);
+  }
+}
+
+
+
+ 
+
+
+
+//***Create Screen
+
+lv_obj_t* create_settings_screen() {
+  lv_obj_t* scr = lv_obj_create(NULL);
+  lv_obj_set_style_bg_color(scr, lv_color_hex(0x000000), 0); //Black
+  //lv_obj_set_style_bg_opa(scr, LV_OPA_COVER, 0);
+  lv_obj_set_style_bg_opa(scr, LV_OPA_COVER, LV_PART_MAIN);
+
+
+
+
+  // Title labels
+
+  lv_obj_t* blokdbitLabel = lv_label_create(scr);
+  lv_label_set_text(blokdbitLabel, "BLOKDBIT");
+  lv_obj_set_style_text_color(blokdbitLabel, lv_color_hex(0xFCA420), 0);
+  lv_obj_set_style_text_font(blokdbitLabel, &lv_font_montserrat_20, 0);
+  lv_obj_align(blokdbitLabel, LV_ALIGN_TOP_RIGHT, -20, 10);
+
+  lv_obj_t* sparkLabel = lv_label_create(scr);
+  lv_label_set_text(sparkLabel, "SPARK");
+  lv_obj_set_style_text_color(sparkLabel, lv_color_hex(0xFFFFFF), 0);
+  lv_obj_set_style_text_font(sparkLabel, &lv_font_montserrat_16, 0);
+  lv_obj_align(sparkLabel, LV_ALIGN_TOP_RIGHT, -20, 35);
+
+  lv_obj_t* priceheightLabel = lv_label_create(scr);
+  lv_label_set_text(priceheightLabel, "Settings");
+  lv_obj_set_style_text_color(priceheightLabel, lv_color_hex(0xFFFFFF), 0);
+  lv_obj_set_style_text_font(priceheightLabel, &lv_font_montserrat_14, 0);
+  lv_obj_align(priceheightLabel, LV_ALIGN_TOP_RIGHT, -20, 60);
+
+
+  
+
+
+
+  //...BUTTONS...
+
+  // Back Button
+  backBtn = lv_obj_create(scr);
+  lv_obj_set_size(backBtn, 21, 21);
+  lv_obj_align(backBtn, LV_ALIGN_LEFT_MID, 40, 0);
+  lv_obj_add_style(backBtn, &orangeStyle, 0);
+  lv_obj_add_event_cb(backBtn, onTouchEvent_settings_screen, LV_EVENT_CLICKED, NULL);
+
+
+  // Right Button
+  rightBtn = lv_obj_create(scr);
+  lv_obj_set_size(rightBtn, 21, 21);
+  lv_obj_align(rightBtn, LV_ALIGN_RIGHT_MID, -30, 0);
+  lv_obj_add_style(rightBtn, &blueStyle, 0);
+  lv_obj_add_event_cb(rightBtn, onTouchEvent_settings_screen, LV_EVENT_CLICKED, NULL);
+  
+
+  return scr;
+}
