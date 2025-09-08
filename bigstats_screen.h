4393a8b161feaa1da3c91122a96b16901cc48afb
BitcoinManor <62268768+BitcoinManor@users.noreply.github.com>
2025-09-08 00:38:39 -0600

    Modify bigstats_screen.h with new extern variables
    Updated header comments and added new extern variables for price labels and change pill.

--- bigstats_screen.h
+++ bigstats_screen.h
@@ -1,6 +1,6 @@
 
-// BLOKDBIT_Spark bigstats_screen.h
-// SPARKv0.03
+// STACKSWORTH_Spark bigstats_screen.h
+// SPARKv0.02
 
 
 #pragma once
@@ -13,6 +13,14 @@ extern bool isRightOn;
 extern lv_style_t orangeStyle;
 extern lv_obj_t* backBtn; // ✅ This tells other files it exists
 extern lv_obj_t* rightBtn;
+extern lv_obj_t* priceValueLabel;
+extern lv_obj_t* priceSatsLabel;
+extern lv_obj_t* satsUsdLabel;
+extern lv_obj_t* satsCadLabel;
+
+extern lv_obj_t* changePillLabel;     // 24h change badge
+extern lv_obj_t* priceChartMini;      // mini sparkline
+extern lv_chart_series_t* priceSeriesMini;
 
 // Shared event handler
 extern void onTouchEvent_bigstats_screen(lv_event_t* e);
@@ -27,3 +35,6 @@ extern lv_style_t greenStyle;
 extern lv_style_t blueStyle;
 
 lv_obj_t* create_bigstats_screen();
+
+// Updates the 24h change pill text + style (green/red)
+void ui_update_change_pill(float changePct);
