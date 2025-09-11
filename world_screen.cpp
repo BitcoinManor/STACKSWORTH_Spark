675fa7c6bdad50d930a82dfc68a58b4f7639a9a7
BitcoinManor <62268768+BitcoinManor@users.noreply.github.com>
2025-09-10 23:59:13 -0600

    Modify labels and adjust button sizes in world_screen
    Changed button size and placement to match other metrics and bigstats screen, Changed Title ato top left and screen to top right

--- world_screen.cpp
+++ world_screen.cpp
@@ -1,6 +1,6 @@
 
-// BLOKDBIT_Spark world_screen.cpp
-// SPARKv0.03
+// STACKSWORTH_Spark world_screen.cpp
+// SPARKv0.0.3
 
 
 #include <Arduino.h>
@@ -57,22 +57,23 @@ lv_obj_t* create_world_screen() {
   // Title labels
 
   lv_obj_t* blokdbitLabel = lv_label_create(scr);
-  lv_label_set_text(blokdbitLabel, "BLOKDBIT");
+  lv_label_set_text(blokdbitLabel, "STACKSWORTH");
   lv_obj_set_style_text_color(blokdbitLabel, lv_color_hex(0xFCA420), 0);
   lv_obj_set_style_text_font(blokdbitLabel, &lv_font_montserrat_20, 0);
-  lv_obj_align(blokdbitLabel, LV_ALIGN_TOP_RIGHT, -20, 10);
+  lv_obj_align(blokdbitLabel, LV_ALIGN_TOP_LEFT, 25, 10);
 
   lv_obj_t* sparkLabel = lv_label_create(scr);
-  lv_label_set_text(sparkLabel, "SPARK");
+  lv_label_set_text(sparkLabel, "// SPARK");
   lv_obj_set_style_text_color(sparkLabel, lv_color_hex(0xFFFFFF), 0);
-  lv_obj_set_style_text_font(sparkLabel, &lv_font_montserrat_16, 0);
-  lv_obj_align(sparkLabel, LV_ALIGN_TOP_RIGHT, -20, 35);
+  lv_obj_set_style_text_font(sparkLabel, &lv_font_montserrat_20, 0);
+  lv_obj_align(sparkLabel, LV_ALIGN_TOP_LEFT, 190, 10);
+
 
   lv_obj_t* priceheightLabel = lv_label_create(scr);
-  lv_label_set_text(priceheightLabel, "World Outside");
-  lv_obj_set_style_text_color(priceheightLabel, lv_color_hex(0xFFFFFF), 0);
-  lv_obj_set_style_text_font(priceheightLabel, &lv_font_montserrat_14, 0);
-  lv_obj_align(priceheightLabel, LV_ALIGN_TOP_RIGHT, -20, 60);
+  lv_label_set_text(priceheightLabel, "WEATHER VIEW");
+  lv_obj_set_style_text_color(priceheightLabel, lv_color_hex(0xFCA420), 0);
+  lv_obj_set_style_text_font(priceheightLabel, &lv_font_montserrat_20, 0);
+  lv_obj_align(priceheightLabel, LV_ALIGN_TOP_RIGHT, -25, 10);
 
 
   
@@ -83,16 +84,16 @@ lv_obj_t* create_world_screen() {
 
   // Back Button
   backBtn = lv_obj_create(scr);
-  lv_obj_set_size(backBtn, 21, 21);
-  lv_obj_align(backBtn, LV_ALIGN_LEFT_MID, 40, 0);
+  lv_obj_set_size(backBtn, 30, 30);
+  lv_obj_align(backBtn, LV_ALIGN_LEFT_MID, 40, 20);
   lv_obj_add_style(backBtn, &orangeStyle, 0);
   lv_obj_add_event_cb(backBtn, onTouchEvent_world_screen, LV_EVENT_CLICKED, NULL);
 
 
   // Right Button
   rightBtn = lv_obj_create(scr);
-  lv_obj_set_size(rightBtn, 21, 21);
-  lv_obj_align(rightBtn, LV_ALIGN_RIGHT_MID, -30, 0);
+  lv_obj_set_size(rightBtn, 30, 30);
+  lv_obj_align(rightBtn, LV_ALIGN_RIGHT_MID, -30, 20);
   lv_obj_add_style(rightBtn, &blueStyle, 0);
   lv_obj_add_event_cb(rightBtn, onTouchEvent_world_screen, LV_EVENT_CLICKED, NULL);
   
